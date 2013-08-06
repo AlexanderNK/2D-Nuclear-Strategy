95d25b803ea737eb7ef8ac14bb0270af47029aa6
Ant0NN <Ant0NN@yandex.ru>
2013-08-06 15:39:35 +0400

    che opyat

--- Assets/Scripts/Factory.cs
+++ Assets/Scripts/Factory.cs
@@ -10,6 +10,13 @@ public class Factory : MonoBehaviour {
 	
 	
 	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			CreateAlcoholics ();
+		}
+		
+	}
 	
+	void CreateAlcoholics (){
+		// GameObject newAlcoholic = Instantiate (obstaclePrefab, new Vector3 (Random.Range (-3, 4), 2,  + 70), transform.rotation) as GameObject;
 	}
 }
