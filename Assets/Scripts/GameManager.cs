d8087266bc4aa55c69b20853880dd855e1689e9f
Ant0NN <Ant0NN@yandex.ru>
2013-08-07 15:48:36 +0400

    some fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,27 @@ public class GameManager : MonoBehaviour
 	public Transform[] waypoints;
 	private MoveAbstainer abstainer;
 	public List<MoveAlcoholics> alcoholics;
+	public int alco = 100;
+	public float alcoBar;
+       
+	void Start ()
+	{
+		alcoBar = Screen.width / 2;
+	}
 	
+	//void Update(){
+	//             addalcoBar(1);
+	//}
+	
+	void OnGUI ()
+	{
+		GUI.Box (new Rect (10, 10, alcoBar, 20), alco.ToString ());
+	}
+
 	public MoveAbstainer Abstainer {
 		get {
 			if (abstainer == null) {
-				abstainer = GameObject.FindGameObjectWithTag("Abstainer").GetComponent<MoveAbstainer> ();
+				abstainer = GameObject.FindGameObjectWithTag ("Abstainer").GetComponent<MoveAbstainer> ();
 			}
 			return abstainer;
 		}
