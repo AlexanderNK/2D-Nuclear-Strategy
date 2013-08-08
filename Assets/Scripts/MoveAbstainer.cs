3fb89fcf6fbae6726070dd7524b1598bc302cb40
AlexanderNK <axelcat2@gmail.ru>
2013-08-08 11:17:28 +0400

    Revert "add alcobar"
    This reverts commit 2d45d683b225968072c30d1be90ee785aade8615.

--- Assets/Scripts/MoveAbstainer.cs
+++ Assets/Scripts/MoveAbstainer.cs
@@ -1,69 +1,65 @@
 ﻿using UnityEngine;
-using UnityEngine;
 using System.Collections;
  
 public class MoveAbstainer : MonoBehaviour
 {
        
-        public float speed = 10f;
-        public float rotationSpeed = 2f;
-        private GameManager gm;
-        public int cur = 0;
-        private int i = 0;
- 
-        void Start ()
-        {
-                gm = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<GameManager> ();
-                gm.checkDir = true;
-           
-        }
+	public float speed = 10f;
+	public float rotationSpeed = 2f;
+	private GameManager gm;
+	public int cur = 0;
+	private int i = 0;
+
+	void Start ()
+	{
+		gm = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<GameManager> ();
+		gm.checkDir = true;
+            
+	}
        
-        void Update ()
-        {
-                //transform.position += new Vector3 (30f * Time.deltaTime, 0, 0);
-                /* if(Input.GetKey("w")){
+	void Update ()
+	{
+		//transform.position += new Vector3 (30f * Time.deltaTime, 0, 0);
+		/* if(Input.GetKey("w")){
                         transform.position += new Vector3(0, 1f, 0);
                 }if ((gm.Abstainer.transform.position - gm.Alcoholics.transform.position).sqrMagnitude < 20) {
-                        Destroy (Alcoholics);
-                }
+			Destroy (Alcoholics);
+		} 
                 if(Input.GetKey("s")){
                         transform.position -= new Vector3(0, 1f, 0);
-                       
+                        
                 }              
                 if(Input.GetKey("a")){
-                        transform.position -= new Vector3(60f*Time.deltaTime, 0, 0);
-                                }
-                                if(Input.GetKey("d")){
-                        transform.position += new Vector3(60f*Time.deltaTime, 0, 0);
-                                }
-                                */
-                               
-                if (cur < gm.waypoints.Length) {
-                        Vector3 dest = gm.waypoints [cur].position;
-                        Vector3 dir = dest - transform.position;
-                        Vector3 nextStep = dir.normalized * speed * Time.deltaTime;
-                       
-                       
-                        if (dir.sqrMagnitude < nextStep.sqrMagnitude) {
-                                if (gm.checkDir) {
-                                        cur++;
-                                } else {
-                                        if (cur > 0) {
-                                                cur--;
-                                                if (i > 1) {
-                                                        gm.checkDir = true;
-                                                        i = 0;
-                                                } else {
-                                                        i++;
-                                                }
-                                        } else {
-                                                gm.checkDir = true;
-                                        }
-                                }
-                        } else {
-                                transform.position += nextStep;
-                               
-                        }
-                }
-        }
+                	transform.position -= new Vector3(60f*Time.deltaTime, 0, 0);
+				}
+				if(Input.GetKey("d")){
+                	transform.position += new Vector3(60f*Time.deltaTime, 0, 0);
+				}
+				*/
+				
+		if (cur < gm.waypoints.Length) {
+			Vector3 dest = gm.waypoints [cur].position;
+			Vector3 dir = dest - transform.position;
+			Vector3 nextStep = dir.normalized * speed * Time.deltaTime;
+			
+			
+			if (dir.sqrMagnitude < nextStep.sqrMagnitude){
+				if (gm.checkDir ){
+					cur++;
+				} else {
+					cur--;
+					if(i>1){
+						gm.checkDir = true;
+						i = 0;
+					} else{
+						i++;
+					}
+				}
+			}
+			else {
+				transform.position += nextStep;
+				
+			}
+		}
+	}
 }
