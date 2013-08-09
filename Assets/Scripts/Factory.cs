44665f21d9519b5a543f9c8fb12980737b1f7d37
Ant0NN <Ant0NN@yandex.ru>
2013-08-09 12:34:23 +0400

    Zero alcohol bug fix

--- Assets/Scripts/Factory.cs
+++ Assets/Scripts/Factory.cs
@@ -1,30 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Factory : MonoBehaviour {
+public class Factory : MonoBehaviour
+{
 	public int men = 9;
 	public const int ALC = 20;
 	public GameObject alcoholic;
 	private GameManager gm;
 	OTSprite myGraphics;
-	void Start () {
+
+	void Start ()
+	{
 		gm = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<GameManager> ();
 		myGraphics = transform.GetComponentInChildren<OTSprite> ();
 	}
 	
-	
-	void Update () {
-		if (myGraphics.clicked && Input.GetMouseButtonDown(0)&& gm.AlcoholAmount>=ALC){
+	void Update ()
+	{
+		if (myGraphics.clicked && Input.GetMouseButtonDown (0) && gm.AlcoholAmount >= ALC && gm.men > 1) {
 			CreateAlcoholics ();
 		}
 		gm.alcoholAmount += Time.deltaTime * men;
 	}
 	
-	void CreateAlcoholics (){
-		GameObject newAlcoholic = Instantiate (alcoholic, gm.waypoints[gm.waypoints.Length-1].position,Quaternion.identity) as GameObject;
-		gm.alcoholAmount-=ALC;
-		if(men >1){
-		men -=1;
-		}
+	void CreateAlcoholics ()
+	{
+		GameObject newAlcoholic = Instantiate (alcoholic, gm.waypoints [gm.waypoints.Length - 1].position, Quaternion.identity) as GameObject;
+		gm.alcoholAmount -= ALC;
+		gm.men--;
 	}
 }
